5ecb013f6086d5fd6cfaacb9d5c914d10c7fd49d
Robin Markowitz <loepoe71@gmail.com>
2018-04-12 10:52:33 -0700

    Render UI elements

--- public/components/cart.jsx
+++ public/components/cart.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const Cart = (props) => (
-  <div className="cart"></div>
+  <div className="cart">
+    <span>0</span>
+  </div>
 );
 
 export default Cart;
--- public/components/quickadd.jsx
+++ public/components/quickadd.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const QuickAdd = (props) => (
-  <div className="quickAdd"></div>
+  <div className="quickAdd">
+    <select className="sizes">
+      <option>Select Size</option>
+    </select>
+    <select className="quantity">
+      <option>QTY: 1</option>
+    </select>
+    <button className="add-btn">ADD TO BAG</button>
+  </div>
 );
 
 export default QuickAdd;
