70d8697ea68ea60c04583fcfa9ab136072e7f167
Robin Markowitz <loepoe71@gmail.com>
2018-04-12 11:33:30 -0700

    Improve visuals

--- public/components/quickadd.jsx
+++ public/components/quickadd.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const QuickAdd = (props) => (
-  <div className="quickAdd">
-    <select className="sizes">
-      <option>Select Size</option>
-    </select>
-    <select className="quantity">
-      <option>QTY: 1</option>
-    </select>
-    <button className="add-btn">ADD TO BAG</button>
+  <div className="quickadd">
+    <div className="quickadd-select">
+      <select className="quickadd-dropdown quickadd-select-sizes">
+        <option>Select Size</option>
+      </select>
+      <select className="quickadd-dropdown quickadd-select-quantity">
+        <option>QTY: 1</option>
+      </select>
+    </div>
+    <div className="quickadd-button">
+      <button className="quickadd-button-add">ADD TO BAG</button>
+    </div>
   </div>
 );
 
