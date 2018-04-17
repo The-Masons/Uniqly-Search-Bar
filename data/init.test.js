b702d58efd7dd299219b02ffeb9d6705dabbbc30
Robin Markowitz <loepoe71@gmail.com>
2018-04-17 12:11:47 -0700

    Write twoField tests

--- data/init.test.js
+++ data/init.test.js
@@ -3,14 +3,14 @@ const initScripts = require('./init.js');
 
 jest.mock('pg-pool');
 
-describe('createTables', () => {
-  beforeEach(() => {
-    Pool.mockClear();
-    Pool.prototype.connect.mockClear();
-    Pool.prototype.mockQuery.mockClear();
-    Pool.prototype.mockRelease.mockClear();
-  });
+beforeEach(() => {
+  Pool.mockClear();
+  Pool.prototype.connect.mockClear();
+  Pool.prototype.mockQuery.mockClear();
+  Pool.prototype.mockRelease.mockClear();
+});
 
+describe('createTables', () => {
   test('should query the db to create each table', () => {
     const tableQueries = [
       `CREATE TABLE IF NOT EXISTS names (
@@ -59,14 +59,55 @@ describe('createTables', () => {
         expect(clientQuery.mock.calls[i][0].split(' ').filter(word => word !== ''))
           .toEqual(tableQueries[i].split(' ').filter(word => word !== ''));
       }
-    })});
+    });
+  });
 
-  test('should release all clients back in the pool', () => {
+  test('should release all clients back into the pool', () => {
     const pool = new Pool();
     const clientRelease = Pool.prototype.mockRelease;
 
-    return initScripts.createTables().then(() => {
-      expect(clientRelease).toHaveBeenCalledTimes(6);
-    });
+    return initScripts.createTables().then(() => expect(clientRelease).toHaveBeenCalledTimes(6));
   });
+});
+
+describe('populateTwoField', () => {
+  test('should correctly query the db to create rows', () => {
+    const pool = new Pool();
+    const clientQuery = Pool.prototype.mockQuery;
+
+    return initScripts.populateTwoField('foo', 'bar', 1).then(() => expect(clientQuery)
+      .toHaveBeenCalledWith('INSERT INTO foos(foo_id, name) VALUES($1, $2)', [0, 'bar 0']));
+  });
+
+  test('should query the db the correct amount of times', () => {
+    const pool = new Pool();
+    const clientQuery = Pool.prototype.mockQuery;
+
+    return initScripts.populateTwoField('foo', 'bar', 10).then(() => expect(clientQuery).toHaveBeenCalledTimes(10));
+  });
+
+  test('should release all clients back into the pool', () => {
+    const pool = new Pool();
+    const clientRelease = Pool.prototype.mockRelease;
+
+    return initScripts.populateTwoField('foo', 'bar', 10).then(() => expect(clientRelease).toHaveBeenCalledTimes(10));
+  });
+});
+
+describe('populateProducts', () => {
+  test('should correctly query the db to create rows', () => {});
+  test('should query the db the correct amount of times', () => {});
+  test('should release all clients back into the pool', () => {});
+});
+
+describe('populateImages', () => {
+  test('should correctly query the db to create rows', () => {});
+  test('should query the db the correct amount of times', () => {});
+  test('should release all clients back into the pool', () => {});
+});
+
+describe('populateProdsSizes', () => {
+  test('should correctly query the db to create rows', () => {});
+  test('should query the db the correct amount of times', () => {});
+  test('should release all clients back into the pool', () => {});
 });
