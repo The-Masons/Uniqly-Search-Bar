736805e9cce5bb8a152373c21f592f521934b25c
Robin Markowitz <loepoe71@gmail.com>
2018-04-17 16:09:02 -0700

    Refactor gets into query

--- db/index.js
+++ db/index.js
@@ -8,38 +8,16 @@ pool.on('error', (err) => {
   process.exit(-1);
 });
 
-const getSizesQtys = (productId, callback) => pool.connect()
-  .then(client => client.query(`
-    SELECT sizes.name, products_sizes.quantity FROM sizes
-    INNER JOIN products_sizes ON sizes.size_id = products_sizes.size_id
-    WHERE products_sizes.product_id = $1 ORDER BY sizes.name;`, [productId])
+const query = (queryText, queryArgs, callback) => pool.connect()
+  .then(client => client.query(queryText, queryArgs)
     .then((res) => {
       client.release();
-      callback(res);
+      callback(null, res);
     })
     .catch((err) => {
       client.release();
-      console.log(err.stack);
+      callback(err, null);
     }))
-  .catch(err => console.log(err.stack));
+  .catch(err => callback(err, null));
 
-const getProductDetails = (productId, sizeId, callback) => pool.connect()
-  .then(client => client.query(`
-    SELECT names.name, colors.name, sizes.name, products.price, images.img_url
-    FROM names, colors, sizes, products, images
-    WHERE products.product_id = $1 AND sizes.size_id = $2
-    AND names.name_id IN (SELECT name_id FROM products WHERE product_id = $1)
-    AND colors.color_id IN (SELECT color_id FROM products WHERE product_id = $1)
-    AND images.product_id = $1;`, [productId, sizeId])
-    .then((res) => {
-      client.release();
-      callback(res);
-    })
-    .catch((err) => {
-      client.release();
-      console.log(err.stack);
-    }))
-  .catch(err => console.log(err.stack));
-
-module.exports.getSizesQtys = getSizesQtys;
-module.exports.getProductDetails = getProductDetails;
+module.exports.query = query;
--- db/index.test.js
+++ db/index.test.js
@@ -10,16 +10,13 @@ beforeEach(() => {
   Pool.prototype.mockRelease.mockClear();
 });
 
-describe('getSizesQtys', () => {
+describe('query', () => {
   test('should correctly query the db', () => {
     const pool = new Pool();
     const clientQuery = Pool.prototype.mockQuery;
-    const expectedQuery = `
-      SELECT sizes.name, products_sizes.quantity FROM sizes
-      INNER JOIN products_sizes ON sizes.size_id = products_sizes.size_id
-      WHERE products_sizes.product_id = $1 ORDER BY sizes.name;`;
+    const expectedQuery = 'SELECT foo FROM bar';
 
-    return db.getSizesQtys(0, data => data).then(() =>
+    return db.query(expectedQuery, [], () => {}).then(() =>
       expect(clientQuery.mock.calls[0][0].split(' ').filter(word => word !== ''))
         .toEqual(expectedQuery.split(' ').filter(word => word !== ''), [0]));
   });
@@ -28,13 +25,13 @@ describe('getSizesQtys', () => {
     const pool = new Pool();
     const mockCallback = jest.fn();
 
-    return db.getSizesQtys(0, mockCallback).then(() => expect(mockCallback).toHaveBeenCalledTimes(1));
+    return db.query('', [], mockCallback).then(() => expect(mockCallback).toHaveBeenCalledTimes(1));
   });
 
   test('should release clients back into the pool', () => {
     const pool = new Pool();
     const clientRelease = Pool.prototype.mockRelease;
 
-    return db.getSizesQtys(0, () => {}).then(() => expect(clientRelease).toHaveBeenCalledTimes(1));
+    return db.query('', [], () => {}).then(() => expect(clientRelease).toHaveBeenCalledTimes(1));
   });
 });
