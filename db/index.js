b421b1920470f5c06ae704af369dcd6287decb37
Robin Markowitz <loepoe71@gmail.com>
2018-04-17 15:57:25 -0700

    Implement getProductDetails

--- db/index.js
+++ db/index.js
@@ -23,4 +23,23 @@ const getSizesQtys = (productId, callback) => pool.connect()
     }))
   .catch(err => console.log(err.stack));
 
+const getProductDetails = (productId, sizeId, callback) => pool.connect()
+  .then(client => client.query(`
+    SELECT names.name, colors.name, sizes.name, products.price, images.img_url
+    FROM names, colors, sizes, products, images
+    WHERE products.product_id = $1 AND sizes.size_id = $2
+    AND names.name_id IN (SELECT name_id FROM products WHERE product_id = $1)
+    AND colors.color_id IN (SELECT color_id FROM products WHERE product_id = $1)
+    AND images.product_id = $1;`, [productId, sizeId])
+    .then((res) => {
+      client.release();
+      callback(res);
+    })
+    .catch((err) => {
+      client.release();
+      console.log(err.stack);
+    }))
+  .catch(err => console.log(err.stack));
+
 module.exports.getSizesQtys = getSizesQtys;
+module.exports.getProductDetails = getProductDetails;
