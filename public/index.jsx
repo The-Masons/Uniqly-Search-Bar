81cd127acb90171ee63cef43caeccef66dfe603c
Robin Markowitz <loepoe71@gmail.com>
2018-04-17 17:20:34 -0700

    Render fake data

--- public/index.jsx
+++ public/index.jsx
@@ -10,38 +10,47 @@ class QuickCart extends React.Component {
     super(props);
     this.state = {
       currentItem: 0,
-      sizes: [],
-      quantities: {},
+      sizes: [
+        'Fake Size 0',
+        'Fake Size 1',
+        'Fake Size 2',
+      ],
+      quantities: {
+        'Fake Size 0': 10,
+        'Fake Size 1': 11,
+        'Fake Size 2': 12,
+      },
+      cart: [],
     };
   }
 
-  componentDidMount() {
-    $.ajax({
-      url: `product/${this.state.currentItem}`,
-      method: 'GET',
-      success: (data) => {
-        this.setState({
-          sizes: data.sizes,
-          quantities: data.quantities,
-        });
-      },
-      error: (err) => {
-        this.setState({
-          sizes: ['ERROR'],
-          quantities: {
-            ERROR: -1,
-          },
-        });
-        console.log('Error', err);
-      },
-    });
-  }
+  // componentDidMount() {
+  //   $.ajax({
+  //     url: `product/${this.state.currentItem}`,
+  //     method: 'GET',
+  //     success: (data) => {
+  //       this.setState({
+  //         sizes: data.sizes,
+  //         quantities: data.quantities,
+  //       });
+  //     },
+  //     error: (err) => {
+  //       this.setState({
+  //         sizes: ['ERROR'],
+  //         quantities: {
+  //           ERROR: -1,
+  //         },
+  //       });
+  //       console.log('Error', err);
+  //     },
+  //   });
+  // }
 
   render() {
     return (
       <div className="quickCart">
         <MiniCart />
-        <QuickAdd />
+        <QuickAdd sizes={this.state.sizes} quantities={this.state.quantities}/>
       </div>
     );
   }
