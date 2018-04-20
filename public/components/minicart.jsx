5048bb78a853b10784902b2b7a33deb33b31c18c
Robin Markowitz <loepoe71@gmail.com>
2018-04-20 13:21:16 -0700

    Add mouseover and empty view

--- public/components/minicart.jsx
+++ public/components/minicart.jsx
@@ -1,30 +1,61 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class MiniCart extends React.Component (
+class MiniCart extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      cart: [];
+      cart: [],
+      cartSize: 0,
+      viewClass: 'minicart-view empty hidden',
+      timeoutID: '',
     };
 
     this.generateMiniCart = this.generateMiniCart.bind(this);
+    this.showCart = this.showCart.bind(this);
   }
 
-  generateMiniCart() {
-    
+  showCart() {
+    const debounce = () => {
+      if (this.state.timeoutID) {
+        clearTimeout(this.state.timeoutID);
+      }
+      const newTimeoutID = setTimeout(() => this.setState({
+        viewClass: this.state.viewClass + ' hidden',
+      }), 5000);
+      this.setState({
+        timeoutID: newTimeoutID,
+      });
+    };
+
+    this.setState({
+      viewClass: this.state.viewClass.replace(' hidden', ''),
+    }, debounce);
+  }
+
+  generateMiniCart(cart) {
+    if (cart.length > 0) {
+
+    } else {
+      return 'YOUR BAG IS EMPTY';
+    }
   }
 
   render() {
     return (
-      <div className="minicart-icon">
-        <span>0</span>
-      </div>
-      <div className="minicart-view">
-        {this.generateMiniCart(this.state.cart)}
+      <div className="minicart">
+        <div
+          className="minicart-icon"
+          onMouseEnter={this.showCart}
+          >
+          <span>{this.state.cartSize}</span>
+        </div>
+        <div className={this.state.viewClass}>
+          {this.generateMiniCart(this.state.cart)}
+        </div>
       </div>
     );
   }
-);
+};
 
 export default MiniCart;
