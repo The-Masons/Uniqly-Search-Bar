6362710ee9bc7457106c2f796eab6e55bc8aa78a
Robin Markowitz <loepoe71@gmail.com>
2018-04-22 16:35:21 -0700

    Change file names to kebab case

--- public/index.jsx
+++ public/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import QuickCart from './components/quickcart.jsx';
+import QuickCart from './components/quick-cart.jsx';
 
 ReactDOM.render(<QuickCart />, document.getElementById('app'));
