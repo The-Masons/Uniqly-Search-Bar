7ac0400df66485b8a75169571241b8011a0ddc22
Robin Markowitz <loepoe71@gmail.com>
2018-04-22 15:05:30 -0700

    Refactor db schema

--- server/index.js
+++ server/index.js
@@ -7,9 +7,25 @@ const port = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '/../client')));
 
+app.get('/products', (req, res) => {
+  db.query(`
+    SELECT names.name, colors.color_name, products.product_id FROM
+      (products INNER JOIN names ON products.name_id = names.name_id)
+      INNER JOIN colors ON products.color_id = colors.color_id
+      ORDER BY products.product_id;
+    `, [], (err, data) => {
+      if (err) {
+        console.log(err);
+        res.send();
+      } else {
+        res.send(data);
+      }
+    })
+});
+
 app.get('/product/:productId', (req, res) => {
   db.query(`
-    SELECT sizes.name, products_sizes.quantity FROM
+    SELECT sizes.size_name, products_sizes.quantity FROM
       (sizes INNER JOIN products_sizes ON sizes.size_id = products_sizes.size_id)
       WHERE products_sizes.product_id = $1 ORDER BY sizes.name;
     `, [req.params.productId], (err, data) => {
