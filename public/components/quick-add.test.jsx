8a1097bc144369c83f2c46c99d576dd64d1e689f
Robin Markowitz <loepoe71@gmail.com>
2018-04-22 23:38:40 -0700

    Add test for addToCart

--- public/components/quick-add.test.jsx
+++ public/components/quick-add.test.jsx
@@ -42,4 +42,25 @@ describe('QuickAdd', () => {
     expect(quickAdd.state('currentSize')).toEqual('Fake Size 0');
     expect(quickAdd.find('.quickadd-select-quantity').children().length).toEqual(10);
   });
+
+  test('should add an item to the cart when "ADD TO BAG" is clicked', () => {
+    const mockAdd = jest.fn();
+    const quickAdd = mount(
+      <QuickAdd sizes={[
+        'Fake Size 0',
+        'Fake Size 1',
+        'Fake Size 2',
+      ]}
+      quantities={{
+        'Fake Size 0': 10,
+        'Fake Size 1': 11,
+        'Fake Size 2': 12,
+      }}
+      addToCart={mockAdd}/>
+    );
+
+    quickAdd.find('.quickadd-btn').simulate('click', {target: {form: [{value: 'Fake Size 0'}, {value: 1}]}});
+
+    expect(mockAdd).toHaveBeenCalledWith('Fake Size 0', 1);
+  });
 });
